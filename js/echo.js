dde1cf072a98385ad9683a4a4d3752f1d6b3b1c5
marlanwood <marlan_wood@yahoo.com>
2015-01-25 16:47:28 -0700

    Update to echo assignment

--- js/echo.js
+++ js/echo.js
@@ -1,6 +1,17 @@
 function echo()
 {
   // ...
+  var i, max = 0;
+  for (i = 0; i < arguments.length; i++)
+  {
+
+	  console.log("'"+arguments[i]+"'");
+//	  if (arguments[i] > max)
+//	  {
+//		  max  = arguments[i];
+//          console.log(arguments[i]); 
+//	  }
+  }
 }
 
 echo();
--- php/echo.php
+++ php/echo.php
@@ -1,10 +1,16 @@
 <?php
 
-function echo()
+function eecho()
 {
-  // ...
+  //print "hello";
+  $numargs = func_num_args();
+  $arg_list = func_get_args();
+  for ($i = 0; $i < $numargs; $i++) {
+        echo "'".$arg_list[$i]."'". "<br />\n";
+  }
 }
+eecho();
+eecho('bla');
+eecho('foo', 'bar', 'baz');
 
-echo();
-echo('bla');
-echo('foo', 'bar', 'baz');
+?>
